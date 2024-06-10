48a50767c1eba57df8202effc299afe1fa9cbbdd
Yasser808 <yassermah002@gmail.com>
2024-06-10 20:55:21 +0100

    INITIAL COMMIT

---
+++ script.js
@@ -0,0 +1,6 @@
+function updateContent() {
+    console.log('Button clicked!'); // This line will show in the console when the button is clicked
+
+    document.getElementById("title").innerText = 'Content updated!';
+    document.getElementById('description').innerText = 'The content has been successfully updated.';
+}
